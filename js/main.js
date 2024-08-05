ac73a022bcfcfa3a16428dc91394f8280bd741f1
Kseniia Pukhova <xenio310390@gmail.com>
2024-08-05 08:46:39 +0300

    add filters

--- js/main.js
+++ js/main.js
@@ -254,6 +254,154 @@ document.querySelectorAll('#popup .close-btn, #overlay').forEach((el) => el.addE
   document.body.classList.remove('popup-open');
 }));
 
+//BEGIN filter accordion
+
+$(".filter__accordion__title").on("click", function (e) {
+  e.preventDefault();
+  var $this = $(this);
+
+  $this.toggleClass("accordion-active");
+  $this.parent().toggleClass("border");
+  $this.next().slideToggle();
+  $(".accordion__arrow", this).toggleClass("minus");
+});
+
+// filter check square
+const elProperties = document.querySelectorAll(".el_properties");
+
+elProperties.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    let square = item.querySelector(".square");
+    square.classList.toggle("active-square");
+  });
+});
+
+// price range slider START
+
+if (!!document.querySelector(".range_container")) {
+  function controlFromInput(fromSlider, fromInput, toInput, controlSlider) {
+    const [from, to] = getParsed(fromInput, toInput);
+    fillSlider(fromInput, toInput, "#C6C6C6", "#3D5397", controlSlider);
+    if (from > to) {
+      fromSlider.value = to;
+      fromInput.value = to;
+    } else {
+      fromSlider.value = from;
+    }
+  }
+
+  function controlToInput(toSlider, fromInput, toInput, controlSlider) {
+    const [from, to] = getParsed(fromInput, toInput);
+    fillSlider(fromInput, toInput, "#C6C6C6", "#3D5397", controlSlider);
+    setToggleAccessible(toInput);
+    if (from <= to) {
+      toSlider.value = to;
+      toInput.value = to;
+    } else {
+      toInput.value = from;
+    }
+  }
+
+  function controlFromSlider(fromSlider, toSlider, fromInput) {
+    const [from, to] = getParsed(fromSlider, toSlider);
+    fillSlider(fromSlider, toSlider, "#C6C6C6", "#3D5397", toSlider);
+    if (from > to) {
+      fromSlider.value = to;
+      fromInput.value = to;
+    } else {
+      fromInput.value = from;
+    }
+  }
+
+  function controlToSlider(fromSlider, toSlider, toInput) {
+    const [from, to] = getParsed(fromSlider, toSlider);
+    fillSlider(fromSlider, toSlider, "#C6C6C6", "#3D5397", toSlider);
+    setToggleAccessible(toSlider);
+    if (from <= to) {
+      toSlider.value = to;
+      toInput.value = to;
+    } else {
+      toInput.value = from;
+      toSlider.value = from;
+    }
+  }
+
+  function getParsed(currentFrom, currentTo) {
+    const from = parseInt(currentFrom.value, 10);
+    const to = parseInt(currentTo.value, 10);
+    return [from, to];
+  }
+
+  function fillSlider(from, to, sliderColor, rangeColor, controlSlider) {
+    const rangeDistance = to.max - to.min;
+    const fromPosition = from.value - to.min;
+    const toPosition = to.value - to.min;
+    controlSlider.style.background = `linear-gradient(
+      to right,
+      ${sliderColor} 0%,
+      ${sliderColor} ${(fromPosition / rangeDistance) * 100}%,
+      ${rangeColor} ${(fromPosition / rangeDistance) * 100}%,
+      ${rangeColor} ${(toPosition / rangeDistance) * 100}%, 
+      ${sliderColor} ${(toPosition / rangeDistance) * 100}%, 
+      ${sliderColor} 100%)`;
+  }
+
+  function setToggleAccessible(currentTarget) {
+    const toSlider = document.querySelector("#toSlider");
+    if (Number(currentTarget.value) <= 0) {
+      toSlider.style.zIndex = 2;
+    } else {
+      toSlider.style.zIndex = 0;
+    }
+  }
+
+  const fromSlider = document.querySelector("#fromSlider");
+  const toSlider = document.querySelector("#toSlider");
+  const fromInput = document.querySelector("#fromInput");
+  const toInput = document.querySelector("#toInput");
+  fillSlider(fromSlider, toSlider, "#C6C6C6", "#3D5397", toSlider);
+  setToggleAccessible(toSlider);
+
+  fromSlider.oninput = () => controlFromSlider(fromSlider, toSlider, fromInput);
+  toSlider.oninput = () => controlToSlider(fromSlider, toSlider, toInput);
+  fromInput.oninput = () =>
+    controlFromInput(fromSlider, fromInput, toInput, toSlider);
+  toInput.oninput = () =>
+    controlToInput(toSlider, fromInput, toInput, toSlider);
+
+  // value of start filter price
+  const rangeStart = document.getElementById("fromSlider"),
+    rangeStartV = document.getElementById("fromInput"),
+    setStartValue = () => {
+      const newValue = Number(
+          ((rangeStart.value - rangeStart.min) * 100) /
+            (rangeStart.max - rangeStart.min)
+        ),
+        newPosition = 10 - newValue * 0.2;
+      rangeStartV.innerHTML = `<span>₴${rangeStart.value}</span>`;
+      rangeStartV.style.left = `calc(${newValue}% - (5px))`;
+    };
+  document.addEventListener("DOMContentLoaded", setStartValue);
+  rangeStart.addEventListener("input", setStartValue);
+
+  // value of start filter price
+  const rangeEnd = document.getElementById("toSlider"),
+    rangeEndV = document.getElementById("toInput"),
+    setEndValue = () => {
+      const newValue = Number(
+          ((rangeEnd.value - rangeEnd.min) * 100) /
+            (rangeEnd.max - rangeEnd.min)
+        ),
+        newPosition = 10 - newValue * 0.2;
+      rangeEndV.innerHTML = `<span>₴${rangeEnd.value}</span>`;
+      rangeEndV.style.left = `calc(${newValue}% - (10px))`;
+    };
+  document.addEventListener("DOMContentLoaded", setEndValue);
+  rangeEnd.addEventListener("input", setEndValue);
+}
+
+// price range slider END
+
 
 
 
